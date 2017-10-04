a6e5bf56ab5c82cb5ec75ce2b87aa5d0b4378661
Kay-Thi-Lwin <Kayt36108@gmail.com>
2017-10-04 09:39:06 +0630

    testing

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php 
+				 		
+				 		echo '
+				 			<form class="panel-group form-horizontal" role="form">
+				 				<div class="panel panel-default>
+				 					<div class="panel-heading">Login </div>
+				 					<div class="panel-body">
+				 						<div class="form-group">
+				 						 	<label for="username" class="control-label col-sm-4">User Name</label>
+				 						 	<div class="col-sm-7">
+				 						 		<input type="text" id="username" placeholder="Insert Email Address" class="form-control">
+				 						 	</div>
+				 						</div>
+				 						<div class="form-group">
+				 							<label for="password" id="password" class="form-control" placeholder="Insert Your Password">
+				 						</div>
+				 						<div class="form-group">
+				 							<div class="col-sm-12">
+				 								<input type="submit" class="btn btn-success btn-block">
+				 							</div>
+										</div>
+				 					</div>
+				 				</div>
+				 			</form>
+				 			';
+
+?>
